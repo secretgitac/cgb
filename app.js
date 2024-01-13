2dd618c5721954e9daa36204590f63e2626a13c4
secretgitac <154335571+secretgitac@users.noreply.github.com>
2024-01-13 13:04:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('button');
     button.addEventListener('mouseover', () => 
     {
-        button.style.left=Math.floor(Math.random()*250+1)+'px';
-        button.style.top=Math.floor(Math.random()*250+1)+'px';
+        button.style.left=Math.floor(Math.random()*90+1)+'%';
+        button.style.top=Math.floor(Math.random()*90+1)+'%';
         
         
     
